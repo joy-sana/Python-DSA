dda3f18653788c87d8c8465dc88bea3b1ee69e0d
Joy Abdin Sanai <zjoy.311@gmail.com>
2024-10-19 00:17:04 +0530

    improved StringDecoder

--- StringDecoder.py
+++ StringDecoder.py
@@ -1,28 +1,27 @@
 # alps = {chr(i): i for i in range(ord('a'), ord('z') + 1)}
 
 str = '10110111'
+str = '111111110111110111111111111011111111111101111111111111110'
 val = ''
 alps = {}
 j = 1
+count = 0
+
+
 for i in range(ord('A'),ord('Z')+1):
     x = {'1'*j : chr(i)}
     j+=1
 
     alps.update(x)
 
-print(alps)
-count = 0
-
 for i in str:
-    if i == 1:
+    if i == '1':
         count+=1
     else:
-        zo = '1'*count
-        k = alps[zo]
-        val += k
+        val += alps['1'*count]
         count = 0
+if count != 0:
+    val += alps['1'*count]  
 
-val += alps['1'*count]
-
-print(char)
+print(val)
         
